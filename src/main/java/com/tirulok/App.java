4f2d54dc0b2127eeff5ba322db7ea45ff6ef6a18
tirulok <mycode.kata@gmail.com>
2020-07-20 11:51:28 +0000

    First Version

---
+++ src/main/java/com/tirulok/App.java
@@ -0,0 +1,13 @@
+package com.tirulok;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
